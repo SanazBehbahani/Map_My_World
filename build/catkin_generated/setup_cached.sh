2e6a83299a3a2d4e26b68b080cfff4ad0b2a737c
Sanaz Behbahani <sanaz.behbahani@gmail.com>
2019-05-17 23:19:25 +0000

    setup simulation with Go_Chase_It project

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/workspace/Where_Am_I_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/workspace/Where_Am_I_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/workspace/Where_Am_I_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/workspace/Where_Am_I_ws/build"
+export PYTHONPATH="/home/workspace/Where_Am_I_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/workspace/Where_Am_I_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/robond/Desktop/roboND/version-control/project2/catkin_ws/src:/home/robond/Desktop/roboND/Go_Chase_It/catkin_ws/src:/home/workspace/Where_Am_I_ws/src:$ROS_PACKAGE_PATH"
--- catkin_ws/devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(ball_chaser_MESSAGE_FILES "")
-set(ball_chaser_SERVICE_FILES "/home/robond/Desktop/roboND/Go_Chase_It/catkin_ws/src/ball_chaser/srv/DriveToTarget.srv")
