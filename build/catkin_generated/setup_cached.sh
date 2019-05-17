90113da95e245d1c02213fea2467537c727cf59b
Sanaz Behbahani <sanaz.behbahani@gmail.com>
2019-05-17 23:33:23 +0000

    create map for MyOfficeWorl.world

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,10 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/workspace/Where_Am_I_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/workspace/Where_Am_I_ws/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/workspace/Where_Am_I_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/workspace/Where_Am_I_ws/build"
-export PYTHONPATH="/home/workspace/Where_Am_I_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/workspace/Where_Am_I_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/robond/Desktop/roboND/version-control/project2/catkin_ws/src:/home/robond/Desktop/roboND/Go_Chase_It/catkin_ws/src:/home/workspace/Where_Am_I_ws/src:$ROS_PACKAGE_PATH"
